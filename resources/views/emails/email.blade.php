630409f8abbd8d88dc2b2b165282a0434142db6d
Javier Antonio Hernandez de Paz <javiercito53@gmail.com>
2017-10-18 01:36:50 -0600

    changes on letter

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -11,21 +11,21 @@
 <b>Presentes.</b><br/>
 <br/><br/>
 <p>
-Reciban un cordial saludo por parte de la organización Guatecambia con el fin de establecer una comunicación colaborativa y de seguimiento dentro del proyecto de monitoreo sobre cumplimiento de la Ley de Acceso a la Información Pública, el cual realizamos conjuntamente con Acción Ciudadana y que cuenta con la colaboración de la Institución del Procurador de los Derechos Humanos y el Ministerio Público.
+Reciban un cordial saludo por parte de Guatecambia y Acción Ciudadana, organizaciones que actualmente están realizando un ejercicio de auditoria social a través de un monitoreo de su portal electrónico sobre el cumplimiento de la Ley de Acceso a la Información Pública, el cual está siendo acompañado por el Procurador de los Derechos Humanos. 
 </p>
 
 <p>
-En este sentido, atentamente le enviamos el informe de monitoreo sobre acceso a la información realizado a la página web de la intitución que representa correspondiente al {{ $round_name }}.
+La institución a la que representa es parte del actual monitoreo, por lo que atentamente le enviamos el informe sobre el cumplimiento de acceso a la información realizado a la página web de la institución, correspondiente al {{ $round_name }}.
 </p>
 
 <p>Puede encontrar la visualización del monitoreo en http://www.infopublicagt.org/sujeto/{{ $subject_id }} </p>
 
 <p>
-A razón del monitoreo realizado, se recomienda tomar en consideración las inconsistencias encontradas, confiando en que se tomen las medidas pertinentes para que la instituciòn cumpla con lo establecido en la ley.
+A razón del monitoreo realizado, se recomienda tomar en consideración las inconsistencias encontradas, confiando en que se tomen las medidas pertinentes para que la institución cumpla con lo establecido en la ley.
 </p>
 
 <p>
-Por lo tanto, hacemos manifestar nuestro compromiso de apoyar a su institución para incrementar el nivel de cumplimiento, poniendo a su disposción nuestra experiencia técnica.  
+Asimismo hacemos manifestar nuestro compromiso de apoyar a su institución para incrementar el nivel de cumplimiento, poniendo a su disposición de manera gratuita nuestra experiencia técnica.  
 </p>
 
 <p>
